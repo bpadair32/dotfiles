64da93fda94deec74d58566d2de00df8c9f84480
Brad Adair <bradadair@gmail.com>
2017-02-16 12:24:11 -0500

    changed file structure

--- defaults-general.sh
+++
@@ -1,8 +0,0 @@
-#####Some bash defaults that work for both Mac and Linux
-
-#Turn on colored output for term and ls
-export CLICOLOR=1
-export LSCOLORS="ExGxBxDxCxEgEdxbxgxcxd"
-
-#Set default editor to VIM instead of VI
-export EDITOR="/usr/local/bin/vim"
--- private-mac.sh
+++
@@ -1,4 +0,0 @@
-#####Mac specific settings that should not be shared
-#IQ-Prod AWS key(s)
-export AWS_ACCESS_KEY_ID='AKIAJQGR6THYMEHH247Q'
-export AWS_SECRET_ACCESS_KEY='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'
